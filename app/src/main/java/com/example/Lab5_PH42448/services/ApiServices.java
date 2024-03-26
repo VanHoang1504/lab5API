19e7f76fe38b0cf59f4ca95c3759c072314855b7
VanHoang1504 <hoangcvph42448@fpt.edu.vn>
2024-03-26 21:58:25 +0700

    lab5

---
+++ app/src/main/java/com/example/Lab5_PH42448/services/ApiServices.java
@@ -0,0 +1,42 @@
+package com.example.Lab5_PH42448.services;
+
+import com.example.Lab5_PH42448.model.Distributor;
+import com.example.Lab5_PH42448.model.Response;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiServices {
+    public static String BASE_URL = "http://10.0.2.2:3000/api/";
+
+    @GET("get-list-distributor")
+        Call<Response<ArrayList<Distributor>>> getListDistributor();
+
+    @GET("search-distributor")
+        Call<Response<ArrayList<Distributor>>> searchDistributor(@Query("key") String key);
+
+    @POST("add-distributor")
+    Call<Response<Distributor>> addDistributor(@Body Distributor distributor);
+
+    @PUT("update-distributor-by-id/{id}")
+    Call<Response<Distributor>> updateDistributor(@Path("id") String id,@Body Distributor distributor);
+
+    @DELETE("destroy-distributor-by-id/{id}")
+    Call<Response<Distributor>> deleteDistributor(@Path("id") String id);
+
+
+
+
+
+
+}
+
+
